e642bfd7c180665848c6e3c332d4ea44c53332cf
Olga Panfilova <gr-olga@ya.ru>
2024-01-28 12:55:00 +0100

    added a bit more styles

--- src/components/BlogForm/BlogForm.tsx
+++ src/components/BlogForm/BlogForm.tsx
@@ -6,24 +6,25 @@ export default function BlogForm() {
     const categories = useRecoilValue(getCategoriesState);
     return (
         <div className={styles.blogForm}>
+            <h2 className={styles.blogForm__title}>Plaats een blog bericht</h2>
             <div className={styles.blogForm__row}>
-                <label htmlFor="title">Berichtnaam</label>
-                <input type="text" placeholder="Geen title"/>
+                <label className={styles.blogForm__label} htmlFor="title">Berichtnaam</label>
+                <input className={styles.blogForm__input} type="text" placeholder="Geen title"/>
             </div>
             <div className={styles.blogForm__row}>
-                <label htmlFor="category">Categorie</label>
-                <select>
+                <label className={styles.blogForm__label} htmlFor="category">Categorie</label>
+                <select className={styles.blogForm__select}>
                     {categories.map((category: string, index: number) => (
                         <option key={index} value={category}>{category}</option>
                     ))}
                 </select>
             </div>
             <div className={styles.blogForm__row}>
-                <label htmlFor="file">Header afbeelding</label>
-                <input type="file"/>
+                <label className={styles.blogForm__label} htmlFor="file">Header afbeelding</label>
+                <input className={styles.blogForm__input} type="file"/>
             </div>
             <div className={styles.blogForm__row}>
-                <input type="text" maxLength={5000} placeholder="Bericht"/>
+                <input className={styles.blogForm__input} type="text" maxLength={5000} placeholder="Bericht"/>
             </div>
             <button>Bericht aanmaken</button>
         </div>
