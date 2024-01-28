ddd931e73f8f08383b4d94dc20a4a8a7972a9d21
Olga Panfilova <gr-olga@ya.ru>
2024-01-28 16:04:41 +0100

    fixed styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import './App.css';
+import style from './App.module.scss';
 import Header from "./components/Header/Header";
 import {Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import BlogPage from "./pages/BlogPage/BlogPage";
-import {getCategoriesState} from "./store";
 
 function App() {
     return (
         <div className="App">
             <Header/>
-            <header className="App-header">
+            <div className={style.container}>
                 <React.Suspense fallback={<div>Loading...</div>}>
                     <Routes>
                         <Route path="/" element={<HomePage/>}/>
                         <Route path="/blog" element={<BlogPage/>}/>
+
                     </Routes>
                 </React.Suspense>
-            </header>
+            </div>
         </div>
     );
 }
