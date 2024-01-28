6cb8150e99592d3c55e0b3e1fb454012cae86770
Olga Panfilova <gr-olga@ya.ru>
2024-01-28 13:48:09 +0100

    added form state

--- src/components/BlogForm/BlogForm.tsx
+++ src/components/BlogForm/BlogForm.tsx
@@ -1,19 +1,47 @@
 import {useRecoilValue} from "recoil";
 import {getCategoriesState} from "../../store";
 import styles from './blogForm.module.scss';
+import {ChangeEvent, FormEvent, useState} from "react";
 
 export default function BlogForm() {
     const categories = useRecoilValue(getCategoriesState);
+
+    const [formData, setFormData] = useState({
+        name: '',
+        category: 'Tech',
+        file: '',
+        message: ''
+    });
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>): void => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+        //TODO: handle submit
+        console.log(formData);
+    };
+
     return (
-        <div className={styles.blogForm}>
+        <form name="blogForm" className={styles.blogForm} onSubmit={handleSubmit}>
             <h2 className={styles.blogForm__title}>Plaats een blog bericht</h2>
             <div className={styles.blogForm__row}>
                 <label className={styles.blogForm__label} htmlFor="title">Berichtnaam</label>
-                <input className={styles.blogForm__input} type="text" placeholder="Geen title"/>
+                <input type="text"
+                       name="name"
+                       className={styles.blogForm__input}
+                       value={formData.name}
+                       onChange={handleChange}
+                       placeholder="Geen title"/>
             </div>
             <div className={styles.blogForm__row}>
                 <label className={styles.blogForm__label} htmlFor="category">Categorie</label>
-                <select className={styles.blogForm__select}>
+                <select name="category"
+                        className={styles.blogForm__select}
+                        value={formData.category}
+                        onChange={handleChange}
+                >
                     {categories.map((category: string, index: number) => (
                         <option key={index} value={category}>{category}</option>
                     ))}
@@ -24,11 +52,17 @@ export default function BlogForm() {
                 <input className={styles.blogForm__input} type="file"/>
             </div>
             <div className={styles.blogForm__row}>
-                <textarea className={styles.blogForm__input} rows={12} placeholder="Bericht"/>
+                <textarea className={styles.blogForm__input}
+                          name="message"
+                          rows={12}
+                          value={formData.message}
+                          onChange={handleChange}
+                          placeholder="Bericht"
+                />
             </div>
             <div className={`${styles.blogForm__row} ${styles.centered}`}>
-                <button className={styles.blogForm__button}>Bericht aanmaken</button>
+                <button type="submit" className={styles.blogForm__button}>Bericht aanmaken</button>
             </div>
-        </div>
+        </form>
     )
 }
